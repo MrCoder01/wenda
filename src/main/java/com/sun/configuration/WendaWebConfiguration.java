bf49a3232e1299f76f958b404ee489e0e34d1c12
ty <star1004079091@qq.com>
2017-04-05 14:48:51 +0800

    拦截器配置

---
+++ src/main/java/com/sun/configuration/WendaWebConfiguration.java
@@ -0,0 +1,29 @@
+package com.sun.configuration;
+
+import com.sun.interceptor.LoginRequiredInterceptor;
+import com.sun.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ty on 2017/3/29.
+ */
+@Component
+public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    PassportInterceptor passportInterceptor;
+
+    @Autowired
+    LoginRequiredInterceptor loginRequiredInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //添加顺序为拦截器执行顺序
+        registry.addInterceptor(passportInterceptor);
+        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/*");
+        super.addInterceptors(registry);
+    }
+}
