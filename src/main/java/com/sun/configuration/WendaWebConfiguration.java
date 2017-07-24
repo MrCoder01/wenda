936e071c17b7470db8544611bf3ab087d4d3f31e
MrCoder01 <star1004079091@163.com>
2017-07-24 22:25:04 +0800

    问答初步系统

--- src/main/java/com/sun/configuration/WendaWebConfiguration.java
+++ src/main/java/com/sun/configuration/WendaWebConfiguration.java
@@ -5,6 +5,7 @@ import com.sun.interceptor.PassportInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -26,4 +27,10 @@ public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
         registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/*");
         super.addInterceptors(registry);
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/upload/**").addResourceLocations("file:D:/var/upload/");
+//        super.addResourceHandlers(registry);
+//    }
 }
